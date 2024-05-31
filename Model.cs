52e6c7bf35bf58390f5e86094f4e99f827677484
Daniel K. Silva <danielks@gmail.com>
2024-05-31 09:15:48 -0300

    Loads and draws the model.

---
+++ Model.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyRasterizer
+{
+    
+
+    internal class Model
+    {
+
+        private List<Vector3> verts;
+        private List<Face> faces;
+
+        public List<Vector3> Verts { get { return verts ; } }
+        public List<Face> Faces { get { return faces; } }
+
+        public Model(string fileName)
+        {
+            verts = new List<Vector3>();            
+            faces = new List<Face>();
+            
+            foreach (var line in File.ReadLines(fileName))
+            {
+                if (line.StartsWith("v "))
+                {
+                    var parts = line.Replace('.', ',').Split(' ');
+
+                    Vector3 v = new Vector3(
+                        Convert.ToSingle(parts[1]),
+                        Convert.ToSingle(parts[2]),
+                        Convert.ToSingle(parts[3]));
+
+                    verts.Add(v);
+                }
+                else if (line.StartsWith("f "))
+                {
+                    var parts = line.Split(' ');
+
+                    var f = new Face();
+
+                    try { 
+
+                        f.Vertex1 = Verts.ElementAt(Convert.ToInt32(parts[1].Split('/')[0])-1);
+                        f.Vertex2 = Verts.ElementAt(Convert.ToInt32(parts[2].Split('/')[0])-1);
+                        f.Vertex3 = Verts.ElementAt(Convert.ToInt32(parts[3].Split('/')[0])-1);
+
+
+                        faces.Add(f);
+
+                    }
+                    catch (Exception ex) { }
+
+
+                }
+            }
+        }
+    }
+
+    struct Face
+    {
+        public Vector3 Vertex1;
+        public Vector3 Vertex2;
+        public Vector3 Vertex3;
+        //public float Normal;
+        //public float Texture;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,11 +6,11 @@ using System.Diagnostics.Metrics;
 using System.Drawing;
 using System.Numerics;
 using System.Runtime.CompilerServices;
-using TinyRaycaster;
+using TinyRasterizer;
 using static System.Net.Mime.MediaTypeNames;
 
-const int RENDER_WIDTH = 2048;
-const int RENDER_HEIGHT = 1024;
+const int RENDER_WIDTH = 2000;
+const int RENDER_HEIGHT = 2000;
 const int WINDOW_WIDTH = RENDER_WIDTH;
 const int WINDOW_HEIGHT = RENDER_HEIGHT;
 
@@ -19,10 +19,13 @@ FrameBuffer frameBuffer = new FrameBuffer(RENDER_WIDTH, RENDER_HEIGHT);
 
 Raylib.InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Tiny Rasterizer");
 
+var model = new TinyRasterizer.Model(@"C:\Users\danie\source\repos\TinyRasterizer\resources\african_head.obj");
+
 long frameCount = 0;
 
 Stopwatch sw = new Stopwatch();
 double frameTime = sw.Elapsed.TotalMilliseconds;
+
 
 
 while (!Raylib.WindowShouldClose())
@@ -76,10 +79,35 @@ void render()
 {
     frameBuffer.Clear();
 
-    frameBuffer.Line(13, 20, 80, 40, Raylib_cs.Color.White);
-    frameBuffer.Line(20, 13, 40, 80, Raylib_cs.Color.Red);
-    frameBuffer.Line(80, 40, 13, 20, Raylib_cs.Color.Red);
+    foreach (var f in model.Faces)
+    {
+        int x1 = Util.float_to_int((f.Vertex1.X + 1) * RENDER_WIDTH / 2);
+        int y1 = Util.float_to_int((f.Vertex1.Y + 1) * RENDER_HEIGHT / 2);
 
+        int x2 = Util.float_to_int((f.Vertex2.X + 1) * RENDER_WIDTH / 2);
+        int y2 = Util.float_to_int((f.Vertex2.Y + 1) * RENDER_HEIGHT / 2);
+
+        int x3 = Util.float_to_int((f.Vertex3.X + 1) * RENDER_WIDTH / 2);
+        int y3 = Util.float_to_int((f.Vertex3.Y + 1) * RENDER_HEIGHT / 2);
+
+        if (x1 >= RENDER_WIDTH || y1 >= RENDER_HEIGHT || x1 < 2 || y1 < 2) continue;
+        if (x2 >= RENDER_WIDTH || y2 >= RENDER_HEIGHT || x2 < 2 || y2 < 2) continue;
+        if (x3 >= RENDER_WIDTH || y3 >= RENDER_HEIGHT || x3 < 2 || y3 < 2) continue;
+
+        
+
+        try { 
+        frameBuffer.Line(x1, y1, x2, y2, Raylib_cs.Color.Black);
+        frameBuffer.Line(x2, y2, x3, y3, Raylib_cs.Color.Black);
+        frameBuffer.Line(x3, y3, x1, y1, Raylib_cs.Color.Black);
+
+        //Raylib.DrawLine(x1, y1, x2, y2, Raylib_cs.Color.Red);
+        //Raylib.DrawLine(x1, y1, x3, y3, Raylib_cs.Color.Blue);
+        //Raylib.DrawLine(x2, y2, x3, y3, Raylib_cs.Color.Yellow);
+        }
+        catch (Exception e) { }
+        
+    }
 
 
     Raylib_cs.Image i2 = new Raylib_cs.Image
@@ -90,8 +118,7 @@ void render()
         Mipmaps = 1
     };
 
-    Texture2D t2 = Raylib.LoadTextureFromImage(i2);
-    
+    Texture2D t2 = Raylib.LoadTextureFromImage(i2);    
 
     unsafe
     {
